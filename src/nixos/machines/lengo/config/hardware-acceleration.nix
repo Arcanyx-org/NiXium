a1ed0d9aa03feeaa8556ac22979e766b5975897e
Jacob Hrbek <kreyren@fsfe.org>
2025-02-27 08:20:09 +0000

    sync

--- src/nixos/machines/lengo/config/hardware-acceleration.nix
+++ src/nixos/machines/lengo/config/hardware-acceleration.nix
@@ -17,13 +17,13 @@ in mkMerge [
 			};
 
 			# AMDVLK
-			hardware.opengl.extraPackages = with pkgs; [
-				amdvlk
-			];
-			# For 32 bit applications
-			hardware.opengl.extraPackages32 = with pkgs; [
-				driversi686Linux.amdvlk
-			];
+				hardware.opengl.extraPackages = with pkgs; [
+					amdvlk
+				];
+				# For 32 bit applications
+				hardware.opengl.extraPackages32 = with pkgs; [
+					driversi686Linux.amdvlk
+				];
 		};
 
 		"24.11" = {
@@ -31,13 +31,13 @@ in mkMerge [
 			hardware.graphics.enable32Bit = true;
 
 			# AMDVLK
-			# hardware.graphics.extraPackages = with pkgs; [
-			# 	amdvlk
-			# ];
-			# # For 32 bit applications
-			# hardware.graphics.extraPackages32 = with pkgs; [
-			# 	driversi686Linux.amdvlk
-			# ];
+				hardware.graphics.extraPackages = with pkgs; [
+					amdvlk
+				];
+				# For 32 bit applications
+				hardware.graphics.extraPackages32 = with pkgs; [
+					driversi686Linux.amdvlk
+				];
 		};
 
 		# FIXME-QA(Krey): Duplicate Code
@@ -46,13 +46,13 @@ in mkMerge [
 			hardware.graphics.enable32Bit = true;
 
 			# AMDVLK
-			hardware.graphics.extraPackages = with pkgs; [
-				amdvlk
-			];
-			# For 32 bit applications
-			hardware.graphics.extraPackages32 = with pkgs; [
-				driversi686Linux.amdvlk
-			];
+				hardware.graphics.extraPackages = with pkgs; [
+					amdvlk
+				];
+				# For 32 bit applications
+				hardware.graphics.extraPackages32 = with pkgs; [
+					driversi686Linux.amdvlk
+				];
 		};
 	}."${lib.trivial.release}" or (throw "Release not implemented: ${lib.trivial.release}")
 ]
