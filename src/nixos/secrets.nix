70682d41d61e274a0de041c4e2991aae00ab1872
Jacob Hrbek <kreyren@fsfe.org>
2025-07-15 22:01:33 +0200

    sync

--- src/nixos/secrets.nix
+++ src/nixos/secrets.nix
@@ -8,7 +8,7 @@ let
 	# Systems
 	flexy-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFSY5vNrQFfnDqBOqse2AHSWY1hIIpZWiBYTdQEIYnV9";
 	ignucius-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWL1P+3Bg7rr3NEW2h0I1bXBZtwCpU3IiruewsUQrcg";
-	lengo-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGMy8eELMVOHriBhBvnCdrbGen5WoiLdZAvbR/5YgupA";
+	lengo-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOVORJbikrudevtNrK023PsAIRIBfQb1xJmmnSiizalR";
 	morph-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFJh5Bd1p4GGCAvNkfoWoflrRIFnoj43b2aMs0GxmULs";
 	mracek-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP8d9Nz64gE+x/+Dar4zknmXMAZXUAxhF1IgrA9DO4Ma";
 	sinnenfreude-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIAXnS4xUPWwjBdKDvvy5OInLbs3oeHUUs5qUsX+fBji";
