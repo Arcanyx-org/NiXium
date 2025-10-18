aa7a74d3fbcf32fa2dcce61e65c0e38f8bd6333c
Jacob Hrbek <kreyren@fsfe.org>
2025-10-18 16:08:43 +0200

    New Service: Nextcloud

--- src/nixos/machines/mracek/default.nix
+++ src/nixos/machines/mracek/default.nix
@@ -14,6 +14,7 @@
 			./services/monero.nix
 			./services/murmur.nix
 			./services/navidrome.nix
+			./services/nextcloud.nix
 			./services/openssh.nix
 			./services/tor.nix
 			./services/vaultwarden.nix
